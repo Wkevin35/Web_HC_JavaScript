7dd99c23a0245b541b2e6ae13be6e8990fffedb4
Kevin <j309028133@gmail.com>
2020-07-05 13:00:24 +0800

    feat():init

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.getElementById("body1").innerHTML = "Hello World";
+
+var a = 20;      //number
+var b = 1.5;     //float
+var c = "text";  //string
+var d = true;    //boolean
+
+console.log(a);
+
+console.log(a+5);
+console.log(a-5);
+console.log(a*5);
+console.log(a/5);
+console.log(a%3);
+console.log(a**3);
